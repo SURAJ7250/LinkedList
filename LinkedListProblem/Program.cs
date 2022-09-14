ff77589fac5bd553dce1ec5d3b335cd0799249b9
surajbridgelabz <surajnsinha@gmail.com>
2022-09-14 18:29:44 +0530

    [suraj] Add: Create a simple linked list

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList Problems Statement");
+            LinkedList list = new LinkedList();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Create LinkedList");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+
+                }
+            }
         }
     }
 }
