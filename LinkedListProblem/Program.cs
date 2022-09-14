5588ab7079f2d20229068ec9ce22cc538d5aad54
surajbridgelabz <surajnsinha@gmail.com>
2022-09-14 17:01:27 +0530

    [suraj] Add: Welcome to LinkedList Problems

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Problems Statement");
+        }
+    }
+}
