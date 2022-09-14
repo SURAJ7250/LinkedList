c756d6d5038b1e0f5816dca62004d0d2c96ce22f
surajbridgelabz <surajnsinha@gmail.com>
2022-09-14 19:24:52 +0530

    [suraj] Add: create Linked List by adding

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -30,6 +30,21 @@ namespace LinkedListProblem
             Console.WriteLine("{0} inserted into linked list", node.data);
 
         }
+        //UC2
+        internal void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;//70
+                head = newNode;//30
+                head.next = temp;//30->70
+            }
+        }
         internal void Display()
         {
             Node temp = this.head;
